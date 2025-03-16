61a5a691d620129ae4d2320fc23ccc527b08457c
AlejandroLara09 <laraquiroga2521@gmail.com>
2025-03-16 16:31:21 -0300

    actualizacion de lista

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
 let amigo = [];
 
 
@@ -6,6 +8,17 @@ let amigo = [];
 function nombreAmigos(){
     let ingresoAmigo = document.getElementById("amigo");
     let amigo = ingresoAmigo.value;
+
+    if (amigo === "") {
+
+    alert("Debes digitar un nombre");
+    return;
+    }
+
+    
+
+
+
 
     console.log(amigo);
 
