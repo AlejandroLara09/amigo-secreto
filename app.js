d372efe9b35dbeb8bb44738c2bfa4d05c9706e87
AlejandroLara09 <laraquiroga2521@gmail.com>
2025-03-16 10:19:32 -0300

    Modificacion de nombres en la funcion id

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigo = [];
+
+
+
+function nombreAmigos(){
+    let ingresoAmigo = document.getElementById("amigo");
+    let amigo = ingresoAmigo.value;
+
+    console.log(amigo);
+
+    
+}
