7e6b1002ab0d3db1d3ddb793d13f434edf46a963
AlejandroLara09 <laraquiroga2521@gmail.com>
2025-03-17 23:19:00 -0300

    Finalizacion de codigo

--- app.js
+++ app.js
@@ -7,20 +7,49 @@ let amigo = [];
 
 function nombreAmigos(){
     let ingresoAmigo = document.getElementById("amigo");
-    let amigo = ingresoAmigo.value;
+    let nombreAmigo = ingresoAmigo.value;
 
-    if (amigo === "") {
+    console.log(nombreAmigo);
 
+    if (nombreAmigo === "") {
+//entregar un aviso en caso de  o ingresar datos
     alert("Debes digitar un nombre");
     return;
     }
+    amigo.push(nombreAmigo);
+   ingresoAmigo.value = "";
+   registroDeAmigos();
 
-    
-
-
-
-
-    console.log(amigo);
-
-    
 }
+
+function registroDeAmigos(){
+ let listaAmigos = document.getElementById("listaAmigos");
+ listaAmigos.innerHTML="";
+ 
+
+ for(let i = 0; i <amigo.length; i++){
+
+    let item = document.createElement("li");
+    item.textContent = amigo[i];
+    listaAmigos.appendChild(item);
+}
+ 
+}
+
+function sortearAmigo() {
+    if (amigo.length === 0) {
+    alert("No hay suficientes amigos para sortear");
+    return;
+    }
+    let amigoElegido = amigo[Math.floor(Math.random() * amigo.length)];
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML= `El amigo secreto elegido es : ${amigoElegido}`;
+
+    let limpiarRegistro = document.getElementById("listaAmigos");
+    limpiarRegistro.innerHTML="";
+}
+
+
+
+
+
